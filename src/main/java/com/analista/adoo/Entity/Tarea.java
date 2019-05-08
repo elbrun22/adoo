f4731aeacc65b47fe9dba6971b1045de9f1643f2
Bruno Conte <elbrun22@gmail.com>
2019-05-07 21:20:58 -0300

    todo tuyo

---
+++ src/main/java/com/analista/adoo/Entity/Tarea.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.analista.adoo.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+/**
+ *
+ * @author nahuel
+ */
+@Entity
+@Table (name = "tareas")
+public class Tarea {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id")
+    private int id;
+    @Column (name = "descrip")
+    private String descripcion;
+    @Column (name = "fecha")
+    private String fecha;
+    
+    @ManyToOne
+    @JoinColumn(name = "id_tareas", referencedColumnName = "id")
+    private Tarea tareas;
+
+    public Tarea getTareas() {
+        return tareas;
+    }
+
+    public void setTareas(Tarea tareas) {
+        this.tareas = tareas;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    
+}
--- src/main/java/com/analista/adoo/Entity/Usuario.java
+++ src/main/java/com/analista/adoo/Entity/Usuario.java
@@ -5,10 +5,62 @@
  */
 package com.analista.adoo.Entity;
 
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 /**
  *
- * @author nahuel
+ * @author bruno
  */
+@Entity
+@Table (name = "usuario")
 public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id")
+    private int id;
+    @Column (name = "nombre")
+    private String nombre;
+    
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "usuario",orphanRemoval = true)
+    private List<Tarea> tarea;
+
+    public void addClases(Tarea tareas) {
+        tarea.add(tareas);
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Tarea> getTarea() {
+        return tarea;
+    }
+
+    public void setTarea(List<Tarea> tarea) {
+        this.tarea = tarea;
+    }
+    
     
 }
